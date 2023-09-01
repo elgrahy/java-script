f0990063a5fe45ca27b18e4ef05a110ab665439f
sohila elgrahy <109896426+elgrahy@users.noreply.github.com>
2023-09-01 13:42:44 +0300

    Add files via upload

---
+++ DataTypesJS/assets/js/main.js
@@ -0,0 +1,32 @@
+
+
+//number
+    var x=2;
+    var y=2.5;
+    console.log(x);
+    console.log(y);
+
+//string
+    var Fname = "sohila";
+    var Lname = "Elgrahy";
+    console.log(Fname +" " + Lname);
+
+
+  //boolen
+     console.log(Fname ==Lname);
+
+  //un defined
+    var x;
+    
+ // array
+    const  skincare = ["sunblock" , "mo" ,"cleancer"];
+    console.log(typeof(skincare));
+
+//obj
+    var obj={name:"ahmed",age:20}
+    console.log(typeof(obj));
+
+
+    var x=2;
+    x+=2;
+   console.log(x);
